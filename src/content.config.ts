c65303e3202e2cfaa7afc703319438cda19b7e9a
MattRStoffel <Matt@MrStoffel.com>
2025-03-04 12:01:35 -0800

    fix: Github :)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,10 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  loader: { pattern: "**/*.md", base: "./content/blog" },
+  loader: async () => {
+    const posts = await import.meta.glob("./content/blog/**/*.md");
+    return Object.keys(posts).map((id) => ({ id }));
+  }
   schema: z.object({
     date: z.coerce.date(),
     tags: z.array(z.string()).default([]),
