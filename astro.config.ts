48002d7ac6275a39295f990ba8b66d2d78e50caa
MattRStoffel <Matt@MrStoffel.com>
2025-03-04 11:49:16 -0800

    fix: Modify gitub account

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,6 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [icon()],
-  site: 'https://MrStoffel.github.io',
+  site: 'https://MattRStoffel.github.io',
   base: 'MrStoffel-site',
 });
