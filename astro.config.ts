bf5641d09191b0c469693fe3930042dbdeb3f332
MattRStoffel <Matt@MrStoffel.com>
2026-05-23 11:54:12 -0700

    something

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
+import wikiLinkPlugin from 'remark-wiki-link';
+
+function toSlug(name: string) {
+  return name.toLowerCase().replace(/\s+/g, '-');
+}
 
 export default defineConfig({
   integrations: [icon()],
   site: 'https://mrstoffel.com',
   //base: 'MrStoffel-site',
+  markdown: {
+    remarkPlugins: [
+      [wikiLinkPlugin, {
+        hrefTemplate: (permalink: string) => `/blog/${permalink}`,
+        pageResolver: (name: string) => [toSlug(name)],
+      }],
+    ],
+  },
 });
